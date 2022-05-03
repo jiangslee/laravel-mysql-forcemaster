4535e28377e32e9f9763d3bbf4468a29ede90a96
jiangsili <jiangsili@qq.com>
2022-05-03 11:45:04 +0800

    Hint /*FORCE_MASTER*/ for laravel mysql读写分离地址

--- src/PackageServiceProvider.php
+++ src/PackageServiceProvider.php
@@ -1,7 +1,11 @@
 <?php
 
-namespace Overtrue\LaravelPackage;
+namespace Jiangslee\LaravelMySqlForceMasterPackage;
 
+use Illuminate\Database\Connection;
+use Illuminate\Database\MySqlConnection;
+use Illuminate\Database\Query\Builder;
+use Illuminate\Database\Query\Grammars\MySqlGrammar;
 use Illuminate\Support\ServiceProvider;
 
 class PackageServiceProvider extends ServiceProvider
@@ -11,19 +15,47 @@ class PackageServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->publishes([
-            \dirname(__DIR__) . '/migrations/' => database_path('migrations'),
-        ], 'migrations');
-
-        if ($this->app->runningInConsole()) {
-            $this->loadMigrationsFrom(\dirname(__DIR__) . '/migrations/');
-        }
     }
 
     public function register()
     {
-//        $this->app->singleton(Package::class, function(){
-//            return new Package();
-//        });
+        // 如何给所有的select语句增加Hint语句，如：“/*FORCE_MASTER*/”
+        // https://learnku.com/laravel/t/66526?#reply222886
+        Builder::macro('forceMaster', function () {
+            $this->force_master = true;
+            return $this;
+        });
+
+        Builder::macro('isForceMaster', function () {
+            return $this->force_master ?? false;
+        });
+
+        Connection::resolverFor('mysql', function ($connection, $database, $prefix, $config) {
+            $connection = new MySqlConnection($connection, $database, $prefix, $config);
+            $connection->setQueryGrammar(new class () extends MySqlGrammar {
+                protected function compileColumns(Builder $query, $columns)
+                {
+                    $sql = parent::compileColumns($query, $columns);
+
+                    if (
+                        // 事务处理中，添加/*FORCE_MASTER*/
+                        $query->connection->transactionLevel() > 0 ||
+                        // 写库的情况下，添加/*FORCE_MASTER*/
+                        $query->useWritePdo ||
+                        // query()->isForceMaster()的情况下，添加/*FORCE_MASTER*/
+                        $query->isForceMaster()
+                    ) {
+                        $sql = '/*FORCE_MASTER*/' . $sql;
+                    }
+
+                    return $sql;
+                }
+            });
+
+            return $connection;
+        });
+
+        // 用例：$sql = DB::table('users')->select('*')->forceMaster()->toSql();
+        // /*FORCE_MASTER*/SELECT * FROM `users`
     }
 }
